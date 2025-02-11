1640a104b0ed0cfa6eb83029803b6fff923e2222
Filip Rokita <filiprokita4@gmail.com>
2025-02-11 15:27:06 +0000

    Add initial models and requirements for Flask application

---
+++ models.py
@@ -0,0 +1,16 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    high_score = db.Column(db.Integer, default=0)
+
+class Question(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    question_text = db.Column(db.String(255), nullable=False)
+    answer_a = db.Column(db.String(100), nullable=False)
+    answer_b = db.Column(db.String(100), nullable=False)
+    answer_c = db.Column(db.String(100), nullable=False)
+    correct_answer = db.Column(db.String(1), nullable=False)
